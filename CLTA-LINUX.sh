72ed3f82a06772a0db250b90e5d27eca5a6d568d
potelillo <99680457+potelillo@users.noreply.github.com>
2022-05-26 13:23:39 +0200

    Update CLTA-LINUX.sh

--- CLTA-LINUX.sh
+++ CLTA-LINUX.sh
@@ -72,3 +72,10 @@ network:
         
 sudo netplan apply
 
+# ----------------  INSTALAR AQUA TRIVY EN UBUNTU 20  ---------------- # 
+
+sudo apt-get install wget apt-transport-https gnupg lsb-release
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
+echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update
+sudo apt-get install trivy
