b0238915aded714cfcdde1a64d647c6726bad5c1
potelillo <99680457+potelillo@users.noreply.github.com>
2022-05-27 14:16:44 +0200

    Update CLTA-LINUX.sh

--- CLTA-LINUX.sh
+++ CLTA-LINUX.sh
@@ -1,14 +1,14 @@
-top -i      # VER RECURSOS UTILIZADOS EN EL SISTEMA
-mdir        # CREAR UNA CARPETA
-nano        # CREAR UN ARCHIVO
-clear       # LIMPIAR LA PANTALLA
-history     # 500 ULTIMOS COMANDOS INTRODUCIDOS
-cd          # MOVERSE ENTRE DIRECTORIOS
-ls          # LISTAR UN DIRECTORIO
-pwd         # MUESTRA EL DIRECTORIO ACTUAL
-rm          # BORRAR ARCHIVO
-rmdir       # BORRAR UN DIRECTORIO
-rmdir -r    # BORRAR UN DIRECTORIO CON ARCHIVOS O DIRECTORIOS DENTRO
+top -i                 # VER RECURSOS UTILIZADOS EN EL SISTEMA
+mdir                   # CREAR UNA CARPETA
+nano                   # CREAR UN ARCHIVO
+clear                  # LIMPIAR LA PANTALLA
+history                # 500 ULTIMOS COMANDOS INTRODUCIDOS
+cd                     # MOVERSE ENTRE DIRECTORIOS
+ls                     # LISTAR UN DIRECTORIO
+pwd                    # MUESTRA EL DIRECTORIO ACTUAL
+rm                     # BORRAR ARCHIVO
+rmdir                  # BORRAR UN DIRECTORIO
+rmdir -r               # BORRAR UN DIRECTORIO CON ARCHIVOS O DIRECTORIOS DENTRO
 tree                   # MUESTRA EL ARBOL DE DIRECTORIOS
 cat                    # VISUALIZAR UN ARCHIVO
 nano                   # VISUALIZAR Y EDITAR UN ARCHIVO
